7644c7c3c5916b98c9f52bd47dc5cf20633b7e06
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2025-11-21 18:45:14 +0100

    ✨ add anonymize

--- src/main/java/org/example/FoodFastUtils.java
+++ src/main/java/org/example/FoodFastUtils.java
@@ -28,4 +28,13 @@ public class FoodFastUtils {
         }
         return result;
     }
+
+    public static String anonymize(String text) {
+        String reversed = "";
+        for (int i = text.length() - 1;  i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+        return reversed;
+
+    }
 }
