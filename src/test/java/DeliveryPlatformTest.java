573a5299fa812bdad14830c0ff311590dbea6bf6
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2025-12-30 22:26:13 +0100

    ✅ add findOrdersByCustomer test

--- src/test/java/DeliveryPlatformTest.java
+++ src/test/java/DeliveryPlatformTest.java
@@ -2,6 +2,7 @@ import org.example.*;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -26,7 +27,33 @@ public class DeliveryPlatformTest {
 
         Order foundOrder = resultId.get();
         assertEquals(order.getId(), foundOrder.getId()); // on vérifie que la cmd retrouvée est bien celle placée
+    }
 
+    @Test
+    void findOrdersByCustomer() {
+        DeliveryPlatform platform = new DeliveryPlatform();
+
+        Dish d1 = new Dish("Pizza", new BigDecimal("10"), DishSize.SMALL);
+        Dish d2 = new Dish("Pizza", new BigDecimal("12.50"), DishSize.LARGE);
+
+        HashMap<Dish, Integer> dishes1 = new HashMap<>();
+        dishes1.put(d1, 1);
+        dishes1.put(d2, 2);
+        Customer c1 = new Customer("1", "Julie", "5 rue Victor Hugo, Marquette-lez-Lille");
+        Order order1 = new Order(dishes1, c1);
+
+
+        HashMap<Dish, Integer> dishes2 = new HashMap<>();
+        dishes2.put(d1, 1);
+        Customer c2 = new Customer("1", "Paul", "1 rue de Béthune, Lille");
+        Order order2 = new Order(dishes2, c2);
+
+        platform.placeOrder(order1);
+        platform.placeOrder(order2);
+
+        List<Order> result = platform.findOrdersByCustomer(c1);
+        assertEquals(1, result.size()); // on vérifie qu'on a bien 1 seule cmd pour le client 1 (c1)
+        assertEquals("Julie", result.get(0).getCustomer().getName()); // on vérifie que le client de la commande est bien Julie
 
     }
 
