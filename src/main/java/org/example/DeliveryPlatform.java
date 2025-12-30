6d46d97d497fa747c8a370ccb6e18f4c86c9954a
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2025-12-30 22:26:29 +0100

    ✨ add findOrdersByCustomer and findOrdersByStatus methods in DeliveryPlatform class

--- src/main/java/org/example/DeliveryPlatform.java
+++ src/main/java/org/example/DeliveryPlatform.java
@@ -1,7 +1,9 @@
 package org.example;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DeliveryPlatform {
     private Map<String, Order> orders = new HashMap<>();
@@ -16,5 +18,13 @@ public class DeliveryPlatform {
     public Optional<Order> findOrderById(String orderId) {
         Order order = orders.get(orderId);
         return Optional.ofNullable(order); // Ici, si orders.get(orderId) renvoie null (pas de cmd avec l'id) alors Optional.ofNullable(order) créera un Optional vide
+    }
+
+    public List<Order> findOrdersByCustomer(Customer customer) {
+        return orders.values().stream().filter(o -> o.getCustomer().equals(customer)).collect(Collectors.toList());
+    }
+
+    List<Order> findOrdersByStatus(OrderStatus status) {
+        return orders.values().stream().filter(o -> o.getOrderStatus().equals(status)).collect(Collectors.toList());
     }
 }
